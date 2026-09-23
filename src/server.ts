77ddfa50e529ae8b417ec782d1c941847c19a083
Youssef <youssef.afify05@eng-st.cu.edu.eg>
2026-09-23 21:47:22 +0300

    modified src/server.ts such that it is ready for deployment, and changed baseUrl in postman_collection.json to the value of the deployment link (Deployment is Done)

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import { createApp } from "./app";
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = Number(process.env.PORT) ?? 3000;
 const app = createApp();
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server API listening on port ${PORT}`);
 });
